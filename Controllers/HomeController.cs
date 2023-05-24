b6e14d6a00086bf478b3f12e65d58690475620c2
MarcRemisa <marc10remisa@gmail.com>
2023-05-24 13:32:42 +0200

    primera actu

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace SimpsonsCharacters.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await httpClient.GetAsync("https://api.sampleapis.com/simpsons/characters");
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var characters = JsonConvert.DeserializeObject<List<SimpsonCharacter>>(json);
+                return View(characters);
+            }
+
+            return View();
+        }
+    }
+
+    public class SimpsonCharacter
+    {
+        public string Name { get; set; }
+        public string genre { get; set; }
+    }
+}
